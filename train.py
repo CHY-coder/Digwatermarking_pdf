8829df667dcf0df309bedad2fb0005b80b408341
chy <1940685132@qq.com>
2024-02-28 15:32:51 +0800

    Forgot add modification of train.py last time.

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from torchvision import transforms
 import torch.onnx
 
 import utils
-from transformer_net import TransformerNet
+from model import Encoder, Decoder, Discriminator
 from vgg import Vgg16
 
 
@@ -31,8 +31,8 @@ def check_paths(args):
 def train(args):
     if args.cuda:
         device = torch.device("cuda")
-    elif args.mps:
-        device = torch.device("mps")
+    # elif args.mps:
+    #     device = torch.device("mps")
     else:
         device = torch.device("cpu")
 
@@ -48,140 +48,86 @@ def train(args):
     train_dataset = datasets.ImageFolder(args.dataset, transform)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size)
 
-    transformer = TransformerNet().to(device)
-    optimizer = Adam(transformer.parameters(), args.lr)
+    encoder = Encoder().to(device)
+    optimizer_en = Adam(encoder.parameters(), args.lr)
     mse_loss = torch.nn.MSELoss()
 
     vgg = Vgg16(requires_grad=False).to(device)
-    style_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.mul(255))
-    ])
-    style = utils.load_image(args.style_image, size=args.style_size)
-    style = style_transform(style)
-    style = style.repeat(args.batch_size, 1, 1, 1).to(device)
 
-    features_style = vgg(utils.normalize_batch(style))
-    gram_style = [utils.gram_matrix(y) for y in features_style]
+    discri = Discriminator(args.image_size, args.image_size).to(device)
+    optimizer_discri = Adam(discri.parameters(), args.lr)
+    criterion = torch.nn.BCELoss()
+
+
+    decoder = Decoder().to(device)
+    optimizer_de = Adam(decoder.parameters(), args.lr)
 
     for e in range(args.epochs):
-        transformer.train()
+        
+        encoder.train()
+        discri.train()
+        decoder.train()
+
         agg_content_loss = 0.
-        agg_style_loss = 0.
         count = 0
-        for batch_id, (x, _) in enumerate(train_loader):
+        for batch_id, (x, message) in enumerate(train_loader):
             n_batch = len(x)
             count += n_batch
-            optimizer.zero_grad()
+
+            optimizer_en.zero_grad()
+            optimizer_discri.zero_grad()
+            optimizer_de.zero_grad()
 
             x = x.to(device)
-            y = transformer(x)
-
+            y = encoder(x, message)
             y = utils.normalize_batch(y)
             x = utils.normalize_batch(x)
 
+            vq_loss = args.vq_weight * mse_loss(y, x)
+
             features_y = vgg(y)
             features_x = vgg(x)
-
-            content_loss = args.content_weight * mse_loss(features_y.relu2_2, features_x.relu2_2)
-
-            style_loss = 0.
-            for ft_y, gm_s in zip(features_y, gram_style):
+            percep_loss = 0
+            for ft_y, ft_x in zip(features_y, features_x):
                 gm_y = utils.gram_matrix(ft_y)
-                style_loss += mse_loss(gm_y, gm_s[:n_batch, :, :])
-            style_loss *= args.style_weight
+                gm_x = utils.gram_matrix(ft_x)
+                percep_loss += mse_loss(gm_y, gm_x)
+            percep_loss = args.percep_weight * percep_loss
 
-            total_loss = content_loss + style_loss
+            discri_x = discri(x)
+            discri_y = discri(y)
+            discri_loss = criterion(discri_x, torch.ones_like(discri_x)) + criterion(discri_y, torch.zeros_like(discri_y))
+            discri_loss = args.A_weight * discri_loss
+
+
             total_loss.backward()
-            optimizer.step()
+            optimizer_en.step()
 
             agg_content_loss += content_loss.item()
-            agg_style_loss += style_loss.item()
 
             if (batch_id + 1) % args.log_interval == 0:
-                mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\tstyle: {:.6f}\ttotal: {:.6f}".format(
+                mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\ttotal: {:.6f}".format(
                     time.ctime(), e + 1, count, len(train_dataset),
                                   agg_content_loss / (batch_id + 1),
-                                  agg_style_loss / (batch_id + 1),
-                                  (agg_content_loss + agg_style_loss) / (batch_id + 1)
+                                  (agg_content_loss) / (batch_id + 1)
                 )
                 print(mesg)
 
             if args.checkpoint_model_dir is not None and (batch_id + 1) % args.checkpoint_interval == 0:
-                transformer.eval().cpu()
+                encoder.eval().cpu()
                 ckpt_model_filename = "ckpt_epoch_" + str(e) + "_batch_id_" + str(batch_id + 1) + ".pth"
                 ckpt_model_path = os.path.join(args.checkpoint_model_dir, ckpt_model_filename)
-                torch.save(transformer.state_dict(), ckpt_model_path)
-                transformer.to(device).train()
+                torch.save(encoder.state_dict(), ckpt_model_path)
+                encoder.to(device).train()
 
     # save model
-    transformer.eval().cpu()
+    encoder.eval().cpu()
     save_model_filename = "epoch_" + str(args.epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
         args.content_weight) + "_" + str(args.style_weight) + ".model"
     save_model_path = os.path.join(args.save_model_dir, save_model_filename)
-    torch.save(transformer.state_dict(), save_model_path)
+    torch.save(encoder.state_dict(), save_model_path)
 
     print("\nDone, trained model saved at", save_model_path)
-
-
-def stylize(args):
-    device = torch.device("cuda" if args.cuda else "cpu")
-
-    content_image = utils.load_image(args.content_image, scale=args.content_scale)
-    content_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.mul(255))
-    ])
-    content_image = content_transform(content_image)
-    content_image = content_image.unsqueeze(0).to(device)
-
-    if args.model.endswith(".onnx"):
-        output = stylize_onnx(content_image, args)
-    else:
-        with torch.no_grad():
-            style_model = TransformerNet()
-            state_dict = torch.load(args.model)
-            # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
-            for k in list(state_dict.keys()):
-                if re.search(r'in\d+\.running_(mean|var)$', k):
-                    del state_dict[k]
-            style_model.load_state_dict(state_dict)
-            style_model.to(device)
-            style_model.eval()
-            if args.export_onnx:
-                assert args.export_onnx.endswith(".onnx"), "Export model file should end with .onnx"
-                output = torch.onnx._export(
-                    style_model, content_image, args.export_onnx, opset_version=11,
-                ).cpu()            
-            else:
-                output = style_model(content_image).cpu()
-    utils.save_image(args.output_image, output[0])
-
-
-def stylize_onnx(content_image, args):
-    """
-    Read ONNX model and run it using onnxruntime
-    """
-
-    assert not args.export_onnx
-
-    import onnxruntime
-
-    ort_session = onnxruntime.InferenceSession(args.model)
-
-    def to_numpy(tensor):
-        return (
-            tensor.detach().cpu().numpy()
-            if tensor.requires_grad
-            else tensor.cpu().numpy()
-        )
-
-    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(content_image)}
-    ort_outs = ort_session.run(None, ort_inputs)
-    img_out_y = ort_outs[0]
-
-    return torch.from_numpy(img_out_y)
-
 
 def main():
     main_arg_parser = argparse.ArgumentParser(description="parser for Digital watermarking")
@@ -190,7 +136,7 @@ def main():
     train_arg_parser = subparsers.add_parser("train", help="parser for training arguments")
     train_arg_parser.add_argument("--epochs", type=int, default=2,
                                   help="number of training epochs, default is 2")
-    train_arg_parser.add_argument("--batch-size", type=int, default=4,
+    train_arg_parser.add_argument("--batch-size", type=int, default=2,
                                   help="batch size for training, default is 4")
     train_arg_parser.add_argument("--dataset", type=str, required=True,
                                   help="path to training dataset(glyph image), the path should point to a folder ")
@@ -204,6 +150,14 @@ def main():
                                   help="set it to 1 for running on GPU, 0 for CPU")
     train_arg_parser.add_argument("--seed", type=int, default=42,
                                   help="random seed for training")
+    train_arg_parser.add_argument("--vq_weight", type=float, default=5,
+                                  help="weight for vq_loss, default is 5")
+    train_arg_parser.add_argument("--percep_weight", type=float, default=0.01,
+                                  help="weight for percep_loss, default is 0.01")
+    train_arg_parser.add_argument("--A_weight", type=float, default=1,
+                                  help="weight for A_loss, default is 1")
+    train_arg_parser.add_argument("--m_weight", type=float, default=1,
+                                  help="weight for m_loss, default is 1")
     train_arg_parser.add_argument("--lr", type=float, default=1e-3,
                                   help="learning rate, default is 1e-3")
     train_arg_parser.add_argument("--log-interval", type=int, default=500,
@@ -217,12 +171,13 @@ def main():
     eval_arg_parser.add_argument("--output-image", type=str, required=True,
                                  help="path for saving the output glyph image")
     eval_arg_parser.add_argument("--model", type=str, required=True,
-                                 help="saved model to be used for adding a digital watermark to glyph image. If file ends in .pth - PyTorch path is used, if in .onnx - Caffe2 path")
+                                 help="saved model to be used for adding a digital watermark to glyph image. "
+                                      "If file ends in .pth - PyTorch path is used, if in .onnx - Caffe2 path")
     eval_arg_parser.add_argument("--cuda", type=int, default=False,
                                  help="set it to 1 for running on cuda, 0 for CPU")
     eval_arg_parser.add_argument("--export_onnx", type=str,
                                  help="export ONNX model to a given file")
-    eval_arg_parser.add_argument('--mps', action='store_true', default=False, help='enable macOS GPU training')
+    # eval_arg_parser.add_argument('--mps', action='store_true', default=False, help='enable macOS GPU training')
 
     args = main_arg_parser.parse_args()
 
@@ -232,14 +187,14 @@ def main():
     if args.cuda and not torch.cuda.is_available():
         print("ERROR: cuda is not available, try running on CPU")
         sys.exit(1)
-    if not args.mps and torch.backends.mps.is_available():
-        print("WARNING: mps is available, run with --mps to enable macOS GPU")
+    # if not args.mps and torch.backends.mps.is_available():
+    #     print("WARNING: mps is available, run with --mps to enable macOS GPU")
 
     if args.subcommand == "train":
         check_paths(args)
         train(args)
-    else:
-        stylize(args)
+    # else:
+    #     stylize(args)
 
 
 if __name__ == "__main__":
